c91bf22b8decfdf9d39a1433b7b2c451e9c1a8ad
Ali Alawsi <87736155+Alawsi@users.noreply.github.com>
2022-03-25 14:32:56 +0300

    Create ExtractDomain.py

---
+++ ExtractDomain.py
@@ -0,0 +1,38 @@
+#Created by : t.me/awsi5
+
+import os
+
+print('Put your emails in "Emails.txt"')
+
+def Extracting(Domain):
+    try:
+        file_check = os.stat("Emails.txt").st_size == 0
+        if file_check == True:
+            print('The file is empty')
+        else:
+            try:
+                print('starting...')
+                file = open('Emails.txt', 'r').read().splitlines()
+                for account in file:
+                    if len(account.split()) == 0:
+                        pass
+                    else:
+                        email = account.split('@')[1]
+                        if email == Domain:
+                            try:
+                                a = account.split(':')[1]
+                                with open(f'{Domain}.txt', 'a') as save:
+                                    save.write(f"{''.join(a.split())}\n")
+
+                            except:
+                                with open(f'{Domain}.txt', 'a') as save:
+                                    save.write(f"{''.join(account.split())}\n")
+
+            except:
+                pass
+    except:
+        print("Error, make sure: - Install Python, put the emails in a file 'Emails.txt'")
+
+Domain = input('type the Domain : ')
+Extracting(Domain)
+input('Press Enter to exit : ')
